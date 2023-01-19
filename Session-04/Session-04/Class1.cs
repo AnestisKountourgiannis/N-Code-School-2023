1bf5865cddca252937bd4e98dabe51260ed8a963
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-01-19 03:49:00 +0200

    session-04#01

---
+++ Session-04/Session-04/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+   public class HelloName
+    {
+       public string Get()
+        {
+            string hello = "Hello";
+            string name = "Anestis";
+            return hello + " " + name;
+        }
+    }
+}
--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Session_04;
+using System.Security.Cryptography.X509Certificates;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HelloName helloName = new HelloName();
+        Console.WriteLine(helloName.Get());
+        Console.ReadLine();
+    }
+}
